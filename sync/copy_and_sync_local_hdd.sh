ab6712d0b77f9a69875c5fcc6d11e0e4bf31d670
_ <_>
2023-12-24 07:16:50 +0100

    very important change in sync script

--- sync/copy_and_sync_local_hdd.sh
+++ sync/copy_and_sync_local_hdd.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+function handle_ctrl_c() {
+    echo "You pressed Ctrl+C!"
+	sudo umount /mnt/hdd \
+    && sudo cryptsetup luksClose hdd    
+    exit 1
+}
+
+trap handle_ctrl_c SIGINT
+
 target_uuid="4217e645-895a-4c89-8cc0-ca117d0f2f7f"
 
 blkid_output=$(blkid)
@@ -22,6 +31,9 @@ cp ~/.bashrc ~/Media/Configs/.bashrc \
 && cp -av ~/.config/picom.conf ~/Media/Configs/picom.conf \
 && sudo cryptsetup luksOpen $device_name hdd \
 && sudo mount /dev/mapper/hdd /mnt/hdd \
-&& rsync -av --delete ~/Media/ /mnt/hdd \
+&& rsync -av --dry-run --delete ~/Media/ /mnt/hdd > /tmp/rsync; grep --color=auto -E "^deleting" /tmp/rsync; read -p "Do you wish to continue? (y/n) " user_response \
+&& if [[ $user_response =~ ^[Yy]$ ]]; then
+    rsync -av --delete ~/Media/ /mnt/hdd
+fi \
 && sudo umount /mnt/hdd \
 && sudo cryptsetup luksClose hdd

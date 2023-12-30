fc1afe5ad3e412209492c68c30da9771cca6de48
_ <_>
2023-12-30 19:42:45 +0100

    important sync change (exlude ISO)

--- sync/copy_and_sync_local_hdd.sh
+++ sync/copy_and_sync_local_hdd.sh
@@ -23,17 +23,35 @@ else
     exit 1
 fi
 
+directories=("/mnt/hdd" "/tmp/sync")
+
+for directory in "${directories[@]}"; do
+    if [ -d "$directory" ]; then
+        echo "Directory $directory already exists."
+    else
+        sudo mkdir -p "$directory"
+
+        if [ $? -eq 0 ]; then
+            echo "Directory $directory created successfully."
+        else
+            echo "Failed to create directory $directory."
+            exit 1
+        fi
+    fi
+done
+
 cp ~/.bashrc ~/Media/Configs/.bashrc \
+&& cp -avp ~/.config/micro/settings.json ~/Media/Configs/micro/settings.json \
 && cp -av ~/.config/i3/config ~/Media/Configs/i3/config \
 && cp -av ~/.config/i3status/config ~/Media/Configs/i3status/config \
-&& cp -av ~/.config/alacritty/alacritty.yml ~/Media/Configs/alacritty/alacritty.yml \
+&& cp -av ~/.config/alacritty/alacritty.toml ~/Media/Configs/alacritty/alacritty.toml \
 && cp -av ~/.config/ranger/rc.conf ~/Media/Configs/ranger/rc.conf \
 && cp -av ~/.config/picom.conf ~/Media/Configs/picom.conf \
 && sudo cryptsetup luksOpen $device_name hdd \
 && sudo mount /dev/mapper/hdd /mnt/hdd \
-&& rsync -av --dry-run --delete --exclude WIKIs ~/Media/ /mnt/hdd > /tmp/rsync; grep --color=auto -E "^deleting" /tmp/rsync; read -p "Do you wish to continue? (y/n) " user_response \
+&& rsync -av --dry-run --delete --exclude WIKIs --exclude ISO ~/Media/ /mnt/hdd > /tmp/rsync; grep --color=auto -E "^deleting" /tmp/rsync; read -p "Do you wish to continue? (y/n) " user_response \
 && if [[ $user_response =~ ^[Yy]$ ]]; then
-    rsync -av --delete --exclude WIKIs ~/Media/ /mnt/hdd
+    rsync -av --delete --exclude WIKIs --exclude ISO ~/Media/ /mnt/hdd
 fi \
 && sudo umount /mnt/hdd \
 && sudo cryptsetup luksClose hdd
--- sync/sync_external_hdd.sh
+++ sync/sync_external_hdd.sh
@@ -2,7 +2,7 @@ device_name="/dev/sdb1"
 
 sudo cryptsetup luksOpen $device_name hdd2 \
 && sudo mount /dev/mapper/hdd2 /mnt/hdd2 \
-&& rsync -av --delete --exclude WIKIs ~/Media/ /mnt/hdd2 \
+&& rsync -av --delete --exclude WIKIs --exclude ISO ~/Media/ /mnt/hdd2 \
 && sudo umount /mnt/hdd2 \
 && sudo cryptsetup luksClose hdd2
 

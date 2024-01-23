763fea0e56b674f5f122a4b0719834fb6a9ad75d
_ <no@email>
2024-01-23 07:08:15 +0100

    massive changes agian

--- ec
+++ ec
@@ -6,6 +6,6 @@ test $# -eq 1 && device="$1"
 
 lol=${device##*/}
 
-sudo umount /dev/mapper/$lol &&\
+sudo umount /dev/mapper/$lol
 sudo cryptsetup luksClose $lol
 
--- sync/copy_configs.sh
+++ sync/copy_configs.sh
@@ -1,8 +1,8 @@
 cp ~/.bashrc ~/Media/Configs/.bashrc \
-&& cp -avp ~/.config/micro/settings.json ~/Media/Configs/micro/settings.json \
-&& cp -avp ~/.config/i3/config ~/Media/Configs/i3/config \
-&& cp -avp ~/.config/i3status/config ~/Media/Configs/i3status/config \
-&& cp -avp ~/.config/alacritty/alacritty.toml ~/Media/Configs/alacritty/alacritty.toml \
-&& cp -avp ~/.config/ranger/rc.conf ~/Media/Configs/ranger/rc.conf \
-&& cp -avp ~/.config/picom.conf ~/Media/Configs/picom.conf \
+&& cp -avp ~/.config/micro/settings.json ~/Media/Configs/config/micro/settings.json \
+&& cp -avp ~/.config/i3/config ~/Media/Configs/config/i3/config \
+&& cp -avp ~/.config/i3status/config ~/Media/Configs/config/i3status/config \
+&& cp -avp ~/.config/alacritty/alacritty.toml ~/Media/Configs/config/alacritty/alacritty.toml \
+&& cp -avp ~/.config/ranger/rc.conf ~/Media/Configs/config/ranger/rc.conf \
+&& cp -avp ~/.config/picom.conf ~/Media/Configs/config/picom.conf \
 && cp -avp /etc/pacman.conf ~/Media/Configs/pacman.conf \

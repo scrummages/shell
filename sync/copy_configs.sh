5bc3c040ab9b10a037661ea4e935b71f5d9c80c9
_ <no@email>
2024-02-02 21:38:35 +0100

    added entire micro config

--- sync/copy_configs.sh
+++ sync/copy_configs.sh
@@ -1,8 +1,8 @@
 cp ~/.bashrc ~/Media/Code/Configs/home/.bashrc \
 && rsync -av --delete ~/.config/fish/ ~/Media/Code/Configs/config/fish/ \
+&& rsync -av --delete --exclude buffers --exclude backups ~/.config/micro/ ~/Media/Code/Configs/config/micro/ \
 && cp -avp ~/.vimrc ~/Media/Code/Configs/home/.vimrc \
 && cp -avp ~/.bash_functions ~/Media/Code/Configs/home/.bash_functions \
-&& cp -avp ~/.config/micro/settings.json ~/Media/Code/Configs/config/micro/settings.json \
 && cp -avp ~/.config/i3/config ~/Media/Code/Configs/config/i3/config \
 && cp -avp ~/.config/i3status/config ~/Media/Code/Configs/config/i3status/config \
 && cp -avp ~/.config/alacritty/alacritty.toml ~/Media/Code/Configs/config/alacritty/alacritty.toml \
